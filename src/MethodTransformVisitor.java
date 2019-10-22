7b4cd98bf53d1e6baa2a03334a1e402442ec762d
iprincipe <56736744+iprincipe@users.noreply.github.com>
2019-10-22 14:58:31 -0500

    Update MethodTransformVisitor.java

--- src/MethodTransformVisitor.java
+++ src/MethodTransformVisitor.java
@@ -10,28 +10,33 @@ class MethodTransformVisitor extends MethodVisitor implements Opcodes {
 	String mName;
 	int line;
 	
-	// I added code from my homework because it looked incomplete
-	// Unsure if you solved it a different way or used the code from the teacher
-	@Override
-    	public void visitLineNumber(int var1, Label var2) {
-		if (this.mv != null) {
-		    this.mv.visitLineNumber(var1, var2);
-		}
-	}
-
-	@Override
-	public void visitLabel(Label var1) {
-	if (this.mv != null) {
-		    this.mv.visitLabel(var1);
-		}
-
-	}
-	// End of code added. Delete if unnecessary
-
 	public MethodTransformVisitor(final MethodVisitor mv, String name) {
 		super(ASM5, mv);
 		this.mName=name;
 	}
+	
+	// I added code from my homework because it looked incomplete
+	// Unsure if you solved it a different way or used the code from the teacher
+	// statement coverage collection
+	@Override
+	public void visitLabel(Label var1) {
+		String lineNumber = "line " + line;
+		mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+		mv.visitLdcInsn( lineNumber+ " executed");
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+		super.visitLabel(var1);
+	}
+
+	@Override
+	public void visitLineNumber(int var1, Label var2) {
+		String lineNumber = "line " + var1;
+		mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+		mv.visitLdcInsn(lineNumber +" executed");
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+		line = var1;
+		super.visitLineNumber(var1, var2);
+	}
+	// End of code added. Delete if unnecessary
 
 	// method coverage collection
 	@Override
