0270d7a8173e6edbff099a99c441921e0b35568c
brandonpike <pikebrandon@att.net>
2019-11-04 23:55:30 -0600

    Update ClassTransformVisitor.java

--- COVERAGE-TOOL/agent/src/main/java/agent/ClassTransformVisitor.java
+++ COVERAGE-TOOL/agent/src/main/java/agent/ClassTransformVisitor.java
@@ -15,17 +15,13 @@ public class ClassTransformVisitor extends ClassVisitor implements Opcodes {
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
         this.className = name;
-
         super.visit(version, access, name, signature, superName, interfaces);
     }
     
     @Override
-    public MethodVisitor visitMethod(final int access, final String name,
-            final String desc, final String signature, final String[] exceptions) {
+    public MethodVisitor visitMethod(final int access, final String name, final String desc, final String signature, final String[] exceptions) {
         MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
         return mv == null ? null : new MethodTransformVisitor(mv, className);
     }
 
 }
-
-
