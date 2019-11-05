fae35edf1ead504780b7793750013825a6624f1c
brandonpike <pikebrandon@att.net>
2019-11-04 23:55:56 -0600

    Update ClassTransformer.java

--- COVERAGE-TOOL/agent/src/main/java/agent/ClassTransformer.java
+++ COVERAGE-TOOL/agent/src/main/java/agent/ClassTransformer.java
@@ -3,16 +3,13 @@ package agent;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
 public class ClassTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
-        if (s.startsWith("org/apache/commons/dbutils") || 
-        		s.startsWith("org/joda/time") || 
-        		"other/Stuff".equals(s)) {
+        if (s.startsWith("org/apache/commons/dbutils") || s.startsWith("org/joda/time")) {
     		ClassReader cr = new ClassReader(bytes);
     		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
     		ClassTransformVisitor ca = new ClassTransformVisitor(cw);
