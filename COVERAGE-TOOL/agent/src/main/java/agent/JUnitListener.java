9d602563067f27896ee1f726d2f728c964416b34
brandonpike <pikebrandon@att.net>
2019-11-04 23:57:31 -0600

    Update JUnitListener.java

--- COVERAGE-TOOL/agent/src/main/java/agent/JUnitListener.java
+++ COVERAGE-TOOL/agent/src/main/java/agent/JUnitListener.java
@@ -6,33 +6,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-
+import java.util.HashMap;
+import java.util.HashSet;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.RunListener;
-
 import it.unimi.dsi.fastutil.ints.IntSet;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 
 public class JUnitListener extends RunListener {
 
 	// @init
 	public void testRunStarted(Description description) throws Exception {
-
-		if (null == CoverageCollector.testCaseCoverages) {
-			CoverageCollector.testCaseCoverages = new Object2ObjectOpenHashMap<String, Object2ObjectOpenHashMap<String, IntSet>>();
+		if (CoverageBank.test_Coverages == null) {
+			CoverageBank.test_Coverages = new HashMap<String, HashMap<String, IntSet>>();
 		}
 	}
 
 	// @Before
 	public void testStarted(Description description) {
-		CoverageCollector.testCaseName = "[TEST] " + description.getClassName() + ":" + description.getMethodName();
-		CoverageCollector.testCaseCoverage = new Object2ObjectOpenHashMap<String, IntSet>();
+		CoverageBank.testName = "[TEST] " + description.getClassName() + ":" + description.getMethodName();
+		CoverageBank.coverage = new HashMap<String, IntSet>();
 	}
 
 	// @After
 	public void testFinished(Description description) {
-		CoverageCollector.testCaseCoverages.put(CoverageCollector.testCaseName, CoverageCollector.testCaseCoverage);
+		CoverageBank.test_Coverages.put(CoverageBank.testName, CoverageBank.coverage);
 	}
 
 	// @Finished
@@ -40,22 +38,20 @@ public class JUnitListener extends RunListener {
 	        File fout = new File("../stmt-cov.txt");
 	        FileOutputStream fos = new FileOutputStream(fout);
 	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-	
-	        StringBuilder builder = new StringBuilder();
-	        for (String testCaseName : CoverageCollector.testCaseCoverages.keySet()) {
-	        	builder.append(testCaseName + "\n");
-	        	Object2ObjectOpenHashMap<String, IntSet> caseCoverage = 
-	        			CoverageCollector.testCaseCoverages.get(testCaseName);
+			HashSet<String> test_bank = new HashSet<String>();
+	        for (String testName : CoverageBank.test_Coverages.keySet()) {
+	        	test_bank.add(testName + "\n");
+	        	HashMap<String, IntSet> caseCoverage = CoverageBank.test_Coverages.get(testName);
 	            for (String className : caseCoverage.keySet()) {
 	            	int[] lines = caseCoverage.get(className).toIntArray();
 	            	Arrays.sort(lines);
 	            	for (int i = 0; i < lines.length; i++) {
-	                	builder.append(className + ":" + lines[i] + "\n");
+	                	test_bank.add(className + ":" + lines[i] + "\n");
 					}
 	            }
 	        }
-	        bw.write(builder.toString());
+			for(String s : test_bank)
+				bw.write(s);
 	        bw.close();
 	}
-
 }
